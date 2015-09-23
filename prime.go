d63b99019699e05dc7c422649f7e5e2cc2a1c13d
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2015-09-23 23:59:15 +0800

    Changing the method to a faster Primers from 2

--- prime.go
+++ prime.go
@@ -3,58 +3,47 @@ package prime
 import (
 	"math"
 	"runtime"
-	"sync"
 )
 
-var p []uint64
-var mutex = &sync.Mutex{}
+var ps []uint64
 
-func is_prime(n uint64) bool {
-	if n == 2 {
-		return true
+func fill(i uint64, max uint64, ready chan bool) {
+	a := 2 * i
+	for a <= max {
+		ps[a] = 1
+		a = a + i
 	}
-	if n%2 == 0 {
-		return false
-	}
-	m := uint64(math.Sqrt(float64(n)))
-	for i := uint64(3); i <= m; i = i + 2 {
-		if n%i == 0 {
-			return false
-		}
-	}
-	return true
-}
-
-func chk(min uint64, max uint64, ready chan bool) {
-	var t []uint64
-	for i := min; i <= max; i++ {
-		if is_prime(i) {
-			t = append(t, i)
-		}
-	}
-	mutex.Lock()
-	p = append(p, t...)
-	mutex.Unlock()
 	<-ready
-
 }
 
 func Primes(max uint64) []uint64 {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	ready := make(chan bool, runtime.NumCPU())
 
-	for i := uint64(2); i <= max; i = i + 10000 {
-		if i+9999 <= max {
-			go chk(i, i+9999, ready)
-		} else {
-			go chk(i, max, ready)
+	ps = make([]uint64, max+1)
+	m := uint64(math.Sqrt(float64(max)))
+	for i := uint64(2); i <= m; i = i + 2 {
+
+		if ps[i] == 0 {
+			go fill(i, max, ready)
+			ready <- true
 		}
-		ready <- true
+		if i == 2 {
+			i = 1
+		}
+
 	}
 
 	for i := 0; i < runtime.NumCPU(); i++ {
 		ready <- true
 	}
 
-	return p
+	var t []uint64
+	m = uint64(len(ps))
+	for i := uint64(2); i < uint64(len(ps)); i++ {
+		if ps[i] == 0 {
+			t = append(t, ps[i])
+		}
+	}
+	return t
 }
