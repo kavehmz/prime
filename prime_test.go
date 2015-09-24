a4c88ca9034bec2037cf8ff852e943d0922be664
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2015-09-24 21:59:48 +0800

    correcting unit test texts

--- prime_test.go
+++ prime_test.go
@@ -7,34 +7,34 @@ import (
 func TestPrimes(t *testing.T) {
 	p := Primes(1000000)
 	if len(p) != 78498 {
-		t.Error("Wrong number of prime numbers lower than 1M ", len(p))
+		t.Error("Wrong number of prime numbers lower than 1M,", len(p))
 	}
 	if p[0] != uint64(2) {
-		t.Error("1st prime number is not 2 ", p[0])
+		t.Error("1st prime number is not 2,", p[0])
 	}
 	if p[1] != uint64(3) {
-		t.Error("2nd prime number is not 2 ", p[1])
+		t.Error("2nd prime number is not 3,", p[1])
 	}
 	if p[25] != uint64(101) {
-		t.Error("25th prime number is not 2 ", p[25])
+		t.Error("25th prime number is not 101,", p[25])
 	}
 	if p[23423] != uint64(267391) {
-		t.Error("23424th prime number is not 267391 ", p[23423])
+		t.Error("23424th prime number is not 267391,", p[23423])
 	}
 	p = Primes(100)
 	if p[24] != uint64(97) {
-		t.Error("23424th prime number is not 267391 ", p[24])
+		t.Error("23424th prime number is not 97,", p[24])
 	}
 	if len(p) != 25 {
-		t.Error("Wrong number of prime numbers lower than 10 ", len(p))
+		t.Error("Wrong number of prime numbers lower than 100,", len(p))
 	}
 	p = Primes(1)
 	if len(p) != 0 {
-		t.Error("Edge case of 1 not correct ", len(p))
+		t.Error("Edge case of 1 not correct,", len(p))
 	}
 	p = Primes(2)
 	if len(p) != 1 {
-		t.Error("Edge case of 2 not correct ", len(p))
+		t.Error("Edge case of 2 not correct,", len(p))
 	}
 }
 
