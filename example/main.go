7ab0c0f57af31ac71622984d7f0a448ed0895d2c
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-02-20 10:12:34 +0800

    reducing the n for non segmented test

--- example/main.go
+++ example/main.go
@@ -26,7 +26,7 @@ func main() {
 	}
 	t := time.Now().UnixNano()
 
-	fmt.Println("number of primes (non-segmented-method):", len(prime.SieveOfEratosthenes(1000000000)))
+	fmt.Println("number of primes (non-segmented-method):", len(prime.SieveOfEratosthenes(1000000)))
 	fmt.Println("number of primes (segmented-method):", len(prime.Primes(1000000)))
 	fmt.Println("seconds it took:", float64(time.Now().UnixNano()-t)/1000000000)
 
