32a48c20b14b3268042b1a46d1469c0369a376de
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2016-02-20 19:02:19 +0800

    both 1G

--- example/main.go
+++ example/main.go
@@ -26,8 +26,8 @@ func main() {
 	}
 	t := time.Now().UnixNano()
 
-	// fmt.Println("number of primes (non-segmented-method):", len(prime.SieveOfEratosthenes(1000000000)))
-	fmt.Println("number of primes (segmented-method):", len(prime.Primes(1000000)))
+	fmt.Println("number of primes (non-segmented-method):", len(prime.SieveOfEratosthenes(1000000000)))
+	fmt.Println("number of primes (segmented-method):", len(prime.Primes(1000000000)))
 	fmt.Println("seconds it took:", float64(time.Now().UnixNano()-t)/1000000000)
 
 	if *memprofile != "" {
